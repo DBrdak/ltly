d1a5ef9be3432b77b1d56dae8a778cf45cd57a1a
DBrdak <brdak02dominik@gmail.com>
2024-09-01 04:21:34 +0200

    aws init

---
+++ aws-deploy.ps1
@@ -0,0 +1,50 @@
+$ltlyStackName = "ltly"
+$ltlyTemplate = ".\Ltly.Lambda\template.yaml"
+$ltlyPackagedTemplate = ".\Ltly.Lambda\packaged-template.yaml"
+
+$preStackName = "ltly-pre"
+$preTemplate = ".\pre-template.yaml"
+
+$awsProfile = "dbrdak-lambda"
+$region = "eu-central-1"
+
+Write-Host "Deploying Pre service..."
+aws cloudformation deploy `
+    --template-file $preTemplate `
+    --stack-name $preStackName `
+    --capabilities CAPABILITY_NAMED_IAM `
+    --region $region `
+    --profile $awsProfile
+
+$s3bucket = (aws cloudformation describe-stacks `
+    --stack-name $preStackName `
+    --query "Stacks[0].Outputs[?OutputKey=='S3BucketName'].OutputValue" `
+    --output text `
+    --region $region `
+    --profile $awsProfile)
+
+if (-not $s3bucket) {
+    Write-Host "Error: Could not retrieve the Name of the S3 bucket. Exiting."
+    exit 1
+}
+
+Write-Host "Packaging Messanger service..."
+sam build --template $ltlyTemplate
+sam package --s3-bucket $s3bucket --output-template-file $ltlyPackagedTemplate
+
+Write-Host "Deploying Messanger service..."
+sam deploy --template-file $ltlyPackagedTemplate `
+    --stack-name $ltlyStackName `
+    --capabilities CAPABILITY_IAM `
+    --region $region `
+    --profile $awsProfile
+
+$url = (aws cloudformation describe-stacks `
+--stack-name $ltlyStackName `
+--query "Stacks[0].Outputs[?OutputKey=='ApiURL'].OutputValue" `
+--output text `
+--region $region `
+--profile $awsProfile)
+
+Write-Host "Successfully deployed" -ForegroundColor Blue
+Write-Host "API URL: $url" -ForegroundColor Green
