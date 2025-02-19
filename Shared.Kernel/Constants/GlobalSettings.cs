d2ad4ae07badcd31195623a6e56fd34a8a66a7e2
DBrdak <brdak02dominik@gmail.com>
2025-02-19 21:24:47 +0100

    new url pattern

--- Shared.Kernel/Constants/GlobalSettings.cs
+++ Shared.Kernel/Constants/GlobalSettings.cs
@@ -6,6 +6,6 @@ public static class GlobalSettings
 {
     public const string Domain = "https://dbrdak.com/s";
     public static readonly Regex UrlPattern = new(
-        @"^(https?:\/\/)?(www\.)?([a-zA-Z0-9\p{L}\p{M}-]+\.)+[a-zA-Z\p{L}]{2,}((\/[\w\p{L}\p{M}\-.~:\/?#\[\]@!$&'()*+,;=%]*)?)$",
+        @"^(https?:\/\/)?(www\.)?([a-zA-Z0-9\p{L}\p{M}-]+\.)+[a-zA-Z\p{L}]{2,}((\/[\w\p{L}\p{M}\-.~:\/?#\[\]@!$&'()*+|,;=%]*)?)$",
         RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
 }
